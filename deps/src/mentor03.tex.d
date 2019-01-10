f89353d38858d9a2b88a7dcb7ae3cbbc98b5fbb3
dzdaniel84 <dzdaniel84@gmail.com>
2019-01-09 20:31:57 -0800

    Compiled mentor 3 worksheet (rough draft)

---
+++ deps/src/mentor03.tex.d
@@ -0,0 +1,3 @@
+../published/disc/mentor03.pdf: src/../topics/riscv/translate.tex src/../topics/riscv/field.tex src/../topics/riscv/conversions.tex src/../topics/riscv/reverse.tex
+
+../published/disc/mentor03_sol.pdf: src/../topics/riscv/translate.tex src/../topics/riscv/field.tex src/../topics/riscv/conversions.tex src/../topics/riscv/reverse.tex
