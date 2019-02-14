a97f44b2af58fb04d4a2925eb54eca3b31c5c383
dzdaniel84 <dzdaniel84@gmail.com>
2019-02-13 23:41:19 -0800

    Added most of worksheet 6

---
+++ deps/src/mentor06.tex.d
@@ -0,0 +1,3 @@
+../published/disc/mentor06.pdf: src/../topics/sds/registers.tex src/../topics/sds/basics.tex src/../topics/sds/explanation.tex src/../topics/sds/registers2.tex
+
+../published/disc/mentor06_sol.pdf: src/../topics/sds/registers.tex src/../topics/sds/basics.tex src/../topics/sds/explanation.tex src/../topics/sds/registers2.tex
