0b87fb6e54596f309492082edc6801b9131154ac
dzdaniel84 <dzdaniel84@gmail.com>
2019-02-13 23:54:20 -0800

    Finished rough draft of worksheet 6

--- deps/src/mentor06.tex.d
+++ deps/src/mentor06.tex.d
@@ -1,3 +1,3 @@
-../published/disc/mentor06.pdf: src/../topics/sds/registers.tex src/../topics/sds/basics.tex src/../topics/sds/explanation.tex src/../topics/sds/registers2.tex
+../published/disc/mentor06.pdf: src/../topics/sds/basics.tex src/../topics/fsm/basics.tex src/../topics/fsm/basics3.tex src/../topics/sds/explanation.tex src/../topics/sds/registers.tex src/../topics/fsm/basics2.tex src/../topics/sds/registers2.tex
 
-../published/disc/mentor06_sol.pdf: src/../topics/sds/registers.tex src/../topics/sds/basics.tex src/../topics/sds/explanation.tex src/../topics/sds/registers2.tex
+../published/disc/mentor06_sol.pdf: src/../topics/sds/basics.tex src/../topics/fsm/basics.tex src/../topics/fsm/basics3.tex src/../topics/sds/explanation.tex src/../topics/sds/registers.tex src/../topics/fsm/basics2.tex src/../topics/sds/registers2.tex
